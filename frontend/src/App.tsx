f2f216a3112c2acedff1935813cc947e48bfb4ab
jaskiratchouhan <jkc64@sfu.ca>
2026-07-08 13:55:52 -0700

    Added react routing, added a component for my authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,106 +1,16 @@
-import { useState } from "react";
-import { ActiveRoomPage } from "./pages/ActiveRoomPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/auth/Authentication";
+import HomePage from "./HomePage";
 import "./App.css";
-
-function makeRandomUser() {
-  const id = crypto.randomUUID();
-
-  const colors = [
-    "#FF8A80",
-    "#FFB74D",
-    "#FFF176",
-    "#81C784",
-    "#4DD0E1",
-    "#64B5F6",
-    "#BA68C8"
-  ];
-
-  const adjectives = [
-  "Anonymous",
-  "Happy",
-  "Chill",
-  "Sneaky",
-  "Brave",
-  "Lucky",
-  "Cosmic",
-  "Jolly",
-  "Quiet",
-  "Wild"
-];
-
-const animals = [
-  "Giraffe",
-  "Panda",
-  "Tiger",
-  "Koala",
-  "Fox",
-  "Otter",
-  "Penguin",
-  "Falcon",
-  "Dolphin",
-  "Bear"
-];
-
-function makeRandomName() {
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const animal = animals[Math.floor(Math.random() * animals.length)];
-  const number = Math.floor(1000 + Math.random() * 9000);
-
-  return `${adjective} ${animal} ${number}`;
-}
-
-  return {
-    id,
-    name: makeRandomName(),
-    color: colors[Math.floor(Math.random() * colors.length)]
-  };
-}
-
-export default function App() {
-  const [user] = useState(makeRandomUser);
-  const params = new URLSearchParams(window.location.search);
-  const roomFromUrl = params.get("room");
-
-  const [roomIdInput, setRoomIdInput] = useState(roomFromUrl ?? "");
-  const [activeRoomId, setActiveRoomId] = useState<string | null>(roomFromUrl);
-  const [shouldCreateRoom, setShouldCreateRoom] = useState(false);
-
-  if (activeRoomId || shouldCreateRoom) {
-    return (
-      <ActiveRoomPage
-        user={user}
-        roomId={activeRoomId}
-        shouldCreateRoom={shouldCreateRoom}
-      />
-    );
-  }
-
-  return (
-    <main className="home-page">
-      <h1>JamSync Live Prototype</h1>
-      <p>You are {user.name}</p>
-
-      <button onClick={() => setShouldCreateRoom(true)}>
-        Create Chat
-      </button>
-
-      <div className="join-box">
-        <input
-          value={roomIdInput}
-          onChange={(e) => setRoomIdInput(e.target.value)}
-          placeholder="Enter room code"
-        />
-
-        <button
-          onClick={() => {
-            if (roomIdInput.trim()) {
-              setActiveRoomId(roomIdInput.trim());
-            }
-          }}
-        >
-          Enter Chat
-        </button>
-      </div>
-    </main>
-  );
+export default function App(){
+    return(
+        <BrowserRouter>
+        <Routes> 
+            <Route path = "/" element = {<Authentication />}></Route>
+            <Route path = "/homepage" element = {<HomePage />}></Route>
+        </Routes>
+        
+        </BrowserRouter>
+        
+    )
 }
---
+++ frontend/src/pages/auth/Authentication.tsx
@@ -0,0 +1,14 @@
+
+export default function Authentication() {
+
+
+    return(
+        <>
+
+        <a href = "http://127.0.0.1:3001/auth/spotify">
+            <button>Connect to Spotify</button>
+        </a>
+        
+        </>
+    )
+}
